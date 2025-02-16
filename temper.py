6b95f0da1c75910e7b372c594b370efaa3f259e1
dorpovop <116513116+dorpovop@users.noreply.github.com>
2025-02-16 17:10:45 +0100

    Update temper.py

--- temper.py
+++ temper.py
@@ -3,7 +3,7 @@ def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
 def fahrenheit_to_celsius(fahrenheit):
-    """Конвертирует температуру из Фаренгейта в Цельсий"""
+    """Конвертирует из Фаренгейта в Цельсий"""
     return (fahrenheit - 32) * 5/9
 
 def main():
